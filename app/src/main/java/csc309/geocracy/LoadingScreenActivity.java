b2dc018e1f6e60d2e20104616a43a0f1d434b92d
Andrew Exton <andrewexton373@gmail.com>
2018-05-02 08:35:45 -0700

    loading screen hang tight notification

--- app/src/main/java/csc309/geocracy/LoadingScreenActivity.java
+++ app/src/main/java/csc309/geocracy/LoadingScreenActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import csc309.geocracy.game.GameActivity;
+import es.dmoral.toasty.Toasty;
 
 public class LoadingScreenActivity extends Activity {
 
@@ -16,7 +18,10 @@ public class LoadingScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_screen);
-        findViewById(R.id.loadingSpinner).setVisibility(View.VISIBLE);
+        ProgressBar loadingSpinner = findViewById(R.id.loadingSpinner);
+        loadingSpinner.setVisibility(View.VISIBLE);
+
+        Toasty.warning(this, "Your world is being created... hang tight!",  WAIT_TIME - 200).show();
 
         new Handler().postDelayed(() -> {
             Intent mainIntent = new Intent(LoadingScreenActivity.this, GameActivity.class);
