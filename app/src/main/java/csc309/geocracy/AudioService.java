ab603dc659876b86e3e79f4d2e90e95b79f16123
Andrew Exton <andrewexton373@gmail.com>
2018-05-03 20:48:33 -0700

    music toggle in settings with EventBus

--- app/src/main/java/csc309/geocracy/AudioService.java
+++ app/src/main/java/csc309/geocracy/AudioService.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class AudioService extends Service {
 
+    private static String TAG = "AUDIO_SERVICE";
+
     private MediaPlayer player;
+    private boolean isEnabled = true;
 
     @Nullable
     @Override
@@ -25,7 +29,20 @@ public class AudioService extends Service {
         player.setVolume(1f, 1f);
         player.start();
 
+        EventBus.subscribe("SET_MUSIC_ENABLED_EVENT", this, e -> enableMusic());
+        EventBus.subscribe("SET_MUSIC_DISABLED_EVENT", this, e -> disableMusic());
+
         return START_STICKY;
+    }
+
+    private void enableMusic() {
+        Log.d(TAG, "ENABLING MUSIC");
+        player.start();
+    }
+
+    private void disableMusic() {
+        Log.d(TAG, "DISABLING MUSIC");
+        player.pause();
     }
 
     @Override
