50e2193fbbf297c3e0182f34c144d425199edfd3
Andrew Exton <aexton@calpoly.edu>
2018-06-05 17:02:13 -0700

    player tests

---
+++ app/src/test/java/csc_cccix/geocracy/PlayerTest.java
@@ -0,0 +1,49 @@
+package csc_cccix.geocracy;
+
+import org.junit.Test;
+
+import csc_cccix.geocracy.Util;
+import csc_cccix.geocracy.game.GameData;
+import csc_cccix.geocracy.game.Player;
+import glm_.vec2.Vec2;
+import glm_.vec3.Vec3;
+import glm_.vec4.Vec4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerTest {
+
+    @Test
+    public void constructor() {
+        int id = 2;
+        Vec3 color = new Vec3(0.0f, 0.0f, 0.0f);
+        Player player = new Player(id, color);
+        assertEquals(id, player.getId());
+        assertEquals(color, player.getColor());
+        assertEquals(0, player.getNArmies());
+        assertTrue(player.getTerritories().isEmpty());
+    }
+
+    @Test
+    public void armyPool() {
+        int id = 2;
+        Vec3 color = new Vec3(0.0f, 0.0f, 0.0f);
+        Player player = new Player(id, color);
+
+        player.setArmyPool(10);
+        assertEquals(10, player.getArmyPool());
+
+        player.addOrRemoveNArmiesToPool(-5);
+        assertEquals(5, player.getArmyPool());
+
+        player.addOrRemoveNArmiesToPool(0);
+        assertEquals(5, player.getArmyPool());
+
+        player.addOrRemoveNArmiesToPool(3);
+        assertEquals(8, player.getArmyPool());
+    }
+
+
+}
