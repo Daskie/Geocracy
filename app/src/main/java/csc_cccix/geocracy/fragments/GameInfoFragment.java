f062322ba11b13e8e7a3b9d9f04825a52b246ba6
Andrew Exton <aexton@calpoly.edu>
2018-06-07 18:24:47 -0700

    copies world seed on touch in game info overlay

--- app/src/main/java/csc_cccix/geocracy/fragments/GameInfoFragment.java
+++ app/src/main/java/csc_cccix/geocracy/fragments/GameInfoFragment.java
@@ -1,14 +1,20 @@
 package csc_cccix.geocracy.fragments;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.view.RxView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +22,7 @@ import java.util.Arrays;
 import csc_cccix.R;
 import csc_cccix.geocracy.adapters.PlayerAdapter;
 import csc_cccix.geocracy.game.Player;
+import es.dmoral.toasty.Toasty;
 
 public class GameInfoFragment extends Fragment {
 
@@ -27,6 +34,14 @@ public class GameInfoFragment extends Fragment {
         Long worldSeed = (Long) getArguments().getLong("worldSeed");
         TextView worldSeedView = view.findViewById(R.id.worldSeed);
         worldSeedView.setText("World Seed: " + worldSeed.toString());
+        RxView.touches(worldSeedView).subscribe((seedView) -> {
+            if (seedView.getAction() == MotionEvent.ACTION_UP) {
+                ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("GEOCRACY_WORLD_SEED", "" + worldSeed);
+                clipboard.setPrimaryClip(clip);
+                Toasty.info(getContext(), "Copied world seed: " + worldSeed).show();
+            }
+        });
 
         Player[] players = (Player[]) getArguments().get("players");
 
