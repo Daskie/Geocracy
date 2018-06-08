5ec975911522d3c1780b49ad4bde366be57ca2b9
Andrew Exton <aexton@calpoly.edu>
2018-06-07 20:53:59 -0700

    optimize imports

--- app/src/main/java/csc_cccix/geocracy/main_menu/MenuActivity.java
+++ app/src/main/java/csc_cccix/geocracy/main_menu/MenuActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
